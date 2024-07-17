eb887e4582bf6b6019503ba9a2a60fd5a97e2e86
Vasily Krakovetsky <vasis123@gmail.com>
2024-07-17 22:43:30 +0300

    fix

--- composables/useTelegramHash.ts
+++ composables/useTelegramHash.ts
@@ -1,37 +1,3 @@
-import {createHmac} from 'crypto';
-import type { TelegramWebAppData } from "@/types/telegram-web-app-data";
-
-export const verifyInitData = (telegramInitData: string): boolean => {
-  const urlParams = new URLSearchParams(telegramInitData);
-
-  const hash = urlParams.get('hash');
-  urlParams.delete('hash');
-  urlParams.sort();
-
-  const dataCheck = [];
-  for (const [key, value] of urlParams.entries()) {
-    dataCheck.push(`${key}=${value}`);
-  }
-  const dataCheckString = dataCheck.join("\n");
-
-  const secret = createHmac('sha256', 'WebAppData').update(process.env.BOT_TOKEN ?? '');
-  const calculatedHash = createHmac('sha256', secret.digest()).update(dataCheckString).digest('hex');
-
-  return calculatedHash === hash;
-}
-
-export function paramsToTgWebAppData(entries: URLSearchParams): TelegramWebAppData {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const result: Record<string, any> = {};
-  for (const [key, value] of entries.entries()) {
-    if (value.startsWith('{')) {
-      result[key] = JSON.parse(value);
-    } else {
-      result[key] = value;
-    }
-  }
-  return result as TelegramWebAppData;
-}
 export const convertTelegramHashToString = () => {
   const documentHash = document.location.hash;
   if (documentHash === '') {
