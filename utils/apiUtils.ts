e9afd0767a3665b9cc18cc4a79c8d9b7f1efd456
Vasily Krakovetsky <vasis123@gmail.com>
2024-07-18 10:09:49 +0300

    bearer

--- utils/apiUtils.ts
+++ utils/apiUtils.ts
@@ -4,12 +4,13 @@ const API_BASE_URL = 'https://manga-viewer-chuker.amvera.io';
 
 export async function fetchWithAuth<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
   const telegramSession = localStorage.getItem('telegramSession');
+  console.log('telegramSession', telegramSession)
   
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
     ...options,
     headers: {
       'Accept': 'application/json',
-      'Authorization': telegramSession ? `Telegram ${telegramSession}` : '',
+      'Authorization': telegramSession ? `Bearer ${telegramSession}` : '',
       ...options.headers,
     }
   });
